af56574a3d99edc889ad98acabf49e974cb603bf
Senashu <saobzy@gmail.com>
2024-03-21 11:57:34 +0100

    fixed ty or mtp exe

--- main.py
+++ main.py
@@ -3,14 +3,33 @@ import vgamepad as vg
 import pymem
 from pymem.process import module_from_name
 import json
+import psutil
+import time
 
 gamepad = vg.VDS4Gamepad()
-mem = pymem.Pymem("Ty.exe")
+
+
+def is_game_running():
+    for process in psutil.process_iter(['name']):
+        if process.name() == 'Mul-Ty-Player.exe' or process.name() == 'TY.exe':
+            return process.name()
+    return None
 
 
 def get_current_level():
-    module = module_from_name(mem.process_handle, "Ty.exe").lpBaseOfDll
-    return mem.read_int(module + 0x280594)
+    game_name = is_game_running()
+    if game_name:
+        try:
+            mem = pymem.Pymem(game_name)
+            module = module_from_name(mem.process_handle, game_name).lpBaseOfDll
+            return mem.read_int(module + 0x280594)
+        except pymem.exception.ProcessNotFound:
+            print("Game process not found. Waiting for it to reopen...")
+            while is_game_running() is None:
+                time.sleep(5)
+            return get_current_level()
+    else:
+        return None
 
 
 def ActionButton():
@@ -22,21 +41,12 @@ def ActionButton():
         gamepad.release_button(button=vg.DS4_BUTTONS.DS4_BUTTON_TRIANGLE)
 
 
-# def SelectButton():
-#    if keyboard.is_pressed(Select):
-#        gamepad.press_button(button=vg.DS4_BUTTONS.DS4_BUTTON_SHARE)
-#        gamepad.update()
-#        print("Pressed Select Button")
-#    else:
-#        gamepad.release_button(button=vg.DS4_BUTTONS.DS4_BUTTON_SHARE)
-
-
 def OutbackMovement(CurrentLevel):
     y_value_float = 0.0
 
     if CurrentLevel == 10:
         if keyboard.is_pressed(Movement):
-            y_value_float -= 1.0
+            y_value_float += 1.0
             print("Outback Movement active")
         else:
             y_value_float = 0.0
@@ -49,14 +59,14 @@ def OutbackMovement(CurrentLevel):
         gamepad.update()
 
 
-# Load settings from settings.json
 with open("settings.json", "r") as f:
     settings = json.load(f)
     Movement = settings["Movement"]
     Action = settings["Action"]
     Select = settings["Select"]
+
 while True:
     CurrentLevel = get_current_level()
-    ActionButton()
-    OutbackMovement(CurrentLevel)
-    # SelectButton()
+    if CurrentLevel is not None:
+        ActionButton()
+        OutbackMovement(CurrentLevel)
